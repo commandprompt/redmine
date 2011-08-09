2e63ee1ca4081042b15836dde61879f5223e8d8e
Alex Shulgin <ash@commandprompt.com>
2011-08-09 10:43:17 +0300

    Fixed attachments handling when receiving a new issue by mail.
    Creates a new journal if there is no one already.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -864,18 +864,19 @@ class Issue < ActiveRecord::Base
 
   # Callback on attachment deletion
   def attachment_added(obj)
-    if @current_journal && !obj.new_record?
-      @current_journal.details << JournalDetail.new(:property => 'attachment', :prop_key => obj.id, :value => obj.filename)
-    end
+    init_journal(User.current) unless @current_journal
+    @current_journal.details << JournalDetail.new(:property => 'attachment', :prop_key => obj.id, :value => obj.filename)
   end
 
   # Callback on attachment deletion
   def attachment_removed(obj)
-    journal = init_journal(User.current)
-    journal.details << JournalDetail.new(:property => 'attachment',
-                                         :prop_key => obj.id,
-                                         :old_value => obj.filename)
-    journal.save
+    init_journal(User.current) unless @current_journal
+    @current_journal.details << JournalDetail.new(:property => 'attachment',
+                                                  :prop_key => obj.id,
+                                                  :old_value => obj.filename)
+    # Currently, this is only triggered from attachments#destroy, so
+    # there's no one else to save the issue's journal.
+    @current_journal.save
   end
 
   # Default assignment based on category
