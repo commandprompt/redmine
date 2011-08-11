0aa4a7f4fed26fb0c237eaf03bd988320cb9bbf1
Alex Shulgin <ash@commandprompt.com>
2011-08-11 10:45:18 +0300

    Fixed setting_label: use 'for' HTML attribute.

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -76,7 +76,9 @@ module SettingsHelper
 
   def setting_label(setting, options={})
     label = options.delete(:label)
-    label != false ? content_tag("label", l(label || "setting_#{setting}")) : ''
+    label != false ? content_tag("label",
+                                 l(label || "setting_#{setting}"),
+                                 :for => "settings_#{setting}") : ''
   end
 
   # Renders a notification field for a Redmine::Notifiable option
