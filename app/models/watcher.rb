f95fed7aabfbc15249581c5636641e1806b93fdc
Alex Shulgin <ash@commandprompt.com>
2012-06-21 04:24:37 -0700

    Allow inactive watchers.

--- app/models/watcher.rb
+++ app/models/watcher.rb
@@ -21,7 +21,6 @@ class Watcher < ActiveRecord::Base
 
   validates_presence_of :user
   validates_uniqueness_of :user_id, :scope => [:watchable_type, :watchable_id]
-  validate :validate_user
 
   # Unwatch things that users are no longer allowed to view
   def self.prune(options={})
@@ -34,12 +33,6 @@ class Watcher < ActiveRecord::Base
       end
       pruned
     end
-  end
-
-  protected
-
-  def validate_user
-    errors.add :user_id, :invalid unless user.nil? || user.active?
   end
 
   private
